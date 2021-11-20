ab54650fb2f91e8d27b05294c0c5fef1b57330f2
Corentin <corentin.zheng@gmail.com>
2021-11-20 13:12:53 +0100

    refactor purchasePlaces

--- tests/unit_tests/test_max_limit_places.py
+++ tests/unit_tests/test_max_limit_places.py
@@ -5,9 +5,9 @@ class MaxLimitPlacesTest(BaseTest):
     def test_purchase_less_than_twelve_places(self):
         response = self.app.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival", "places": 3}, follow_redirects=True)
         assert response.status_code == 200
-        assert b'You cannot buy more than your available places or more than 12 places!' not in response.data
+        assert b'You cannot buy more than 12 places!' not in response.data
 
     def test_purchase_more_than_twelve_places(self):
-        response = self.app.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival", "places": 30}, follow_redirects=True)
+        response = self.app.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival", "places": 13}, follow_redirects=True)
         assert response.status_code == 200
-        assert b'You cannot buy more than your available places or more than 12 places!' in response.data
+        assert b'You cannot buy more than 12 places!' in response.data
