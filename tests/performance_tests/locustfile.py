e3cfffbea9a2d40ce5f61643ed59a5788208358c
Corentin <corentin.zheng@gmail.com>
2021-11-26 09:45:05 +0100

    refactor code

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -9,6 +9,14 @@ class ProjectPerfTest(HttpUser):
     @task
     def login(self):
         self.client.post("/showSummary", data={"email": "john@simplylift.co"})
+    
+    @task
+    def get_book(self):
+        self.client.get('/book/Spring Festival/Simply Lift')
+    
+    @task
+    def purchase_place(self):
+        self.client.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival", "places": 3})
 
     @task
     def logout(self):
--- tests/unit_tests/test_purchase_places.py
+++ tests/unit_tests/test_purchase_places.py
@@ -3,7 +3,6 @@ from tests.base import BaseTest
 
 class PurchaseTest(BaseTest):
     def test_purchase_places_with_negative_number(self):
-
         response = self.app.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival", "places": 30}, follow_redirects=True)
         assert response.status_code == 200
         assert b'You cannot buy more than your available places' in response.data
