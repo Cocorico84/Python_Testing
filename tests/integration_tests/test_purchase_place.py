b3b7af09421b7b1b4558c53c644c3cf75f32660b
Corentin <corentin.zheng@gmail.com>
2021-11-28 09:38:29 +0100

    add intgration purchase place test

---
+++ tests/integration_tests/test_purchase_place.py
@@ -0,0 +1,11 @@
+from tests.base import BaseTest
+
+
+class PurchaseTest(BaseTest):
+    def test_purchase_place(self):
+        self.app.get('/')
+        self.app.post('/showSummary', data={"email": "john@simplylift.co"}, follow_redirects=True)
+
+        response = self.app.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival", "places": 3}, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'You cannot buy more than your available places' not in response.data
